7b609a4e8b43de7853a3db1051b234b195eb8afd
Victor Rodrigues <vitumail@gmail.com>
2016-07-18 22:54:50 +0200

    Replace :random => :rand

--- lib/exq/support/randomize.ex
+++ lib/exq/support/randomize.ex
@@ -3,11 +3,11 @@ defmodule Exq.Support.Randomize do
   @on_load :reseed_generator
 
   def reseed_generator do
-    :random.seed(:os.timestamp())
+    :rand.seed(:os.timestamp())
     :ok
   end
 
   def random(number) do
-    :random.uniform(number)
+    :rand.uniform(number)
   end
 end
